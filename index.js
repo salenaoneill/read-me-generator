209bada2adb2784eb1a3e8f58e87bb8ab708b939
Salena O'Neill <salenaoneill@salenas-air.lan>
2023-01-03 13:24:53 -0600

    generate readme

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+// TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-let questions = [
+// TODO: Create an array of questions for user input
+inquirer.prompt([
     {
         type: 'input',
         message: 'what is the title of your project?',
@@ -32,6 +35,43 @@ let questions = [
         message: 'write tests for your application here.',
         name: 'tests'
 
+    },
+    {
+        type: 'checkbox',
+        message: 'choose a license.',
+        choices: [
+            'None',
+            'Apache License 2.0',
+            'GNU General Public License v3.0',
+            'MIT License', 
+            'BSD 2-Clause "Simplified" License', 
+            'BSD 3-Clause "New" or "Revised" License', 
+            'Boost Software License 1.0',
+            'Creative Commons Zero v1.0 Universal',
+            'Eclipse Public License 2.0',
+            'GNU Affero General Public License v3.0',
+            'GNU General Public License v2.0',
+            'GNU Lesser General Public License v2.1',
+            'Mozilla Public License 2.0',
+            'The Unlicense'
+         ],
+         name: 'license'
     }
-]
+])
 
+
+.then((data) => {
+// TODO: Create a function to write README file
+   const readmeData = generateMarkdown(data);
+    function writeToFile(fileName, data) {
+        fs.writeFile(fileName, data, null, (err) =>
+        err ? console.error(err) : console.log("Your ReadMe has been generated"))
+    }
+    writeToFile("README.md", readmeData);
+})
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
