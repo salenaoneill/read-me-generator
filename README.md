cdc5f0d881001b4f24268e75e2dd40d0cc39aaa6
Salena O'Neill <salenaoneill@salenas-air.lan>
2023-01-04 11:46:28 -0600

    license & questions

---
+++ README.md
@@ -0,0 +1,35 @@
+
+  # poop
+  
+  ![Badge](https://img.shields.io/badge/License-Apache-blue.svg)
+
+  ## Description
+  poop
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#Usage)
+  - [License](#license)
+  - [Contribution](#contribution)
+  - [Tests](#Tests)
+
+  ## Installation
+    poopinde
+
+  ## Usage
+    poop
+
+  ## License
+    Apache license. 
+
+  ## Contribution 
+    poop
+
+  ## Tests
+    poop
+
+  ## Questions
+  If you have any questions, contact me!
+  Github: https://github.com/poop
+  Email: poop
+  
--- index.js
+++ index.js
@@ -41,21 +41,23 @@ inquirer.prompt([
         message: 'choose a license.',
         choices: [
             'None',
-            'Apache License 2.0',
-            'GNU General Public License v3.0',
-            'MIT License', 
-            'BSD 2-Clause "Simplified" License', 
-            'BSD 3-Clause "New" or "Revised" License', 
-            'Boost Software License 1.0',
-            'Creative Commons Zero v1.0 Universal',
-            'Eclipse Public License 2.0',
-            'GNU Affero General Public License v3.0',
-            'GNU General Public License v2.0',
-            'GNU Lesser General Public License v2.1',
-            'Mozilla Public License 2.0',
-            'The Unlicense'
+            'Apache',
+            'Apache 2',
+            'MIT',
+            'GPL',
+            'BSD'
          ],
          name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'what is your github username?',
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: 'what is your email address?',
+        name: 'email'
     }
 ])
 
